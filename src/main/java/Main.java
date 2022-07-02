0edb4218e14560d445371ff4c5c8eb4faa8a275f
nikito <aa@aa.aa>
2022-07-02 10:21:03 +0300

    add sum function in third branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,4 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
+
+    static int sum(int b, int c){
+        return  b+c;
+    }
 }
